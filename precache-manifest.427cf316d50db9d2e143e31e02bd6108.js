f3a161a4284d8f66aeb52de91fad232e0e82ce5a
Tyler <theadley2022@pingry.org>
2023-05-27 01:00:09 -0400

    Updates

---
+++ precache-manifest.427cf316d50db9d2e143e31e02bd6108.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef77de8775c484233b71344f95cfee3",
+    "url": "/Lesson-Site/index.html"
+  },
+  {
+    "revision": "daa221fc5fbd40d26dcb",
+    "url": "/Lesson-Site/static/css/main.7f54f458.chunk.css"
+  },
+  {
+    "revision": "97f2d51bb086817e5e46",
+    "url": "/Lesson-Site/static/js/2.ffa174da.chunk.js"
+  },
+  {
+    "revision": "a6b2627ef3558a57dc6bd2077d763681",
+    "url": "/Lesson-Site/static/js/2.ffa174da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa221fc5fbd40d26dcb",
+    "url": "/Lesson-Site/static/js/main.f52347b8.chunk.js"
+  },
+  {
+    "revision": "efb646befe62a360b8cd",
+    "url": "/Lesson-Site/static/js/runtime-main.8443ce04.js"
+  },
+  {
+    "revision": "e104aae712e79b3491f9b74daf4dcf4b",
+    "url": "/Lesson-Site/static/media/logo_nobackground.e104aae7.png"
+  },
+  {
+    "revision": "c5cf57036eb260df3ed6f84a2da65cf3",
+    "url": "/Lesson-Site/static/media/socp-img9.c5cf5703.png"
+  }
+]);
